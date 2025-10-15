ce3a0621f0dc3b26cc9c0d58267f41139d87d6b9
Jagtarsingh42 <jagtarsekhon47@gmail.com>
2025-10-15 14:18:23 -0400

    midterm exam

---
+++ src/status/StatusUser.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+/**
+ * MidTerm-Exam
+ * @author srinivsi
+ */
+
+public enum StatusUser {
+    ZERO("REJECTED"),
+    ONE("PENDING"),
+    TWO("PROCESSING"),
+    THREE("APPROVED"),
+    FOUR("COMPLETED");
+
+    private final String message;
+
+    StatusUser(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    void statusDetail(StatusUser selected) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
+ 
